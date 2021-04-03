01cb644d6f9f55bd28ca3493c7bd327086c09a51
sanidhyajain1 <sanidhyajain077@gmail.com>
2021-04-03 10:13:27 +0530

    username's first letter cannot be a digit

--- public/js/register.js
+++ public/js/register.js
@@ -5,9 +5,13 @@ document.querySelector(".btn").addEventListener("click",function(e){
     e.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+
     if(/\s/g.test(username)){
         alert("username cannot contain spaces");
-    }else if(username.length>12 || username.length<3){
+    }else if(!username.charAt(0) != '_' && !username.charAt(0) != '-' &&  username.charAt(0).toUpperCase() == username.charAt(0).toLowerCase()){
+        alert("username must start with a underscore , hyphen or a letter");
+    }
+    else if(username.length>12 || username.length<3){
         alert("username must be between 3 to 12 letters long");
     }else if(format.test(username)){
         alert("username can only contain alphabets,number,dashes and underscores");
